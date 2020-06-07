2c8d52bc3993258bdbc9933195e5def0ecc82742
Taisuke Fukuno <fukuno@jig.jp>
2020-06-07 10:03:09 +0900

    remove use code

--- geocode.mjs
+++ geocode.mjs
@@ -1,4 +1,4 @@
-import { getLGCode, findCity } from "https://code4sabae.github.io/lgcode/lgcode.mjs";
+import { getLGCode } from "https://code4sabae.github.io/lgcode/lgcode.mjs";
 
 const geocodecache = {};
 const getGeocode = async code => {
@@ -17,9 +17,9 @@ const getGeocode = async code => {
   return data;
 };
 
-const getLatLng = async (prefname, cityname, chome) => {
+const getLatLng = async (name1, name2, chome) => { // 福井県 鯖江市、札幌市 中央区、など2つ指定
   if (chome == null) { return null; }
-  const code = getLGCode(prefname, cityname);
+  const code = getLGCode(name1, name2);
   if (!code) { return null; }
   const citygeo = await getGeocode(code);
   if (!citygeo) { return null; }
